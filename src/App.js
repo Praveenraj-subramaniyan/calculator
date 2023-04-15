5728388c36f0b8d8f872beb7f746d0d912974470
spr887011 <spr887011@gmail.com>
2023-04-15 18:34:16 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, seValue] = useState("");
+  function onChange(event)
+  {
+    const target = event.target;
+    const targetvalue = target.value;
+     if(targetvalue ==="c"){
+      seValue("")
+     }
+     else if(targetvalue === "Backspace")
+     {
+      seValue(value.slice(0, -1))
+     }
+    console.log(value)
+  }
+  function handleKeyDown(event)
+  {
+    const key = event.key;
+    if(event.keyCode >= 48 && event.keyCode <= 57)
+    {
+      seValue(value + `${key}`)
+      console.log(`${key}`)
+    }
+    else if(event.keyCode === 8)
+    {
+     seValue(value.slice(0, -1))
+    }
+    else{
+      alert("Only number are allowed")
+    }
+  }
+  function isEqual(){
+    try{
+      seValue(eval(value))
+    }
+    catch{
+      alert("Octal literals are not allowed")
+    }
+    
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid firstContainer">
+      <div className="container calwin">
+        <div className="row inputdiv ">
+          <div className="col-12">
+            <input className="inputbox " value={value} onKeyDown={(e) => handleKeyDown(e)}/>
+          </div>
+        </div>
+        <div className="row buttondiv">
+          <div className="col-3">
+            <button className="btn c" value="c" onClick={onChange}>C</button>
+          </div>
+          <div className="col-3">
+            <button className="btn" value="Backspace" onClick={onChange}><i className="fa-solid fa-arrow-left fa-sm"></i></button>
+          </div>
+          <div className="col-3">
+            <button className="btn" value="." onClick={() =>seValue(value + ".")}>.</button>
+          </div>
+          <div className="col-3">
+            <button className="btn operators" value="*" onClick={() =>seValue(value + "*")}>X</button>
+          </div>
+        </div>
+        <div className="row buttondiv">
+          <div className="col-3">
+            <button className="btn" value={7} onClick={() =>seValue(value + "7")}>7</button>
+          </div>
+          <div className="col-3">
+            <button className="btn" value={8} onClick={() =>seValue(value + "8")}>8</button>
+          </div>
+          <div className="col-3">
+            <button className="btn" value={9} onClick={() =>seValue(value + "9")}>9</button>
+          </div>
+          <div className="col-3">
+            <button className="btn operators" value="/" onClick={() =>seValue(value + "/")}>/</button>
+          </div>
+        </div>
+        <div className="row buttondiv">
+          <div className="col-3">
+            <button className="btn" value={4} onClick={() =>seValue(value + "4")}>4</button>
+          </div>
+          <div className="col-3">
+            <button className="btn" value={5} onClick={() =>seValue(value + "5")}>5</button>
+          </div>
+          <div className="col-3">
+            <button className="btn" value={6} onClick={() =>seValue(value + "6")}>6</button>
+          </div>
+          <div className="col-3">
+            <button className="btn operators" value="-" onClick={() =>seValue(value + "-")}>-</button>
+          </div>
+        </div>
+        <div className="row buttondiv">
+          <div className="col-3">
+            <button className="btn" value={1} onClick={() =>seValue(value + "1")}>1</button>
+          </div>
+          <div className="col-3">
+            <button className="btn" value={2} onClick={() =>seValue(value + "2")}>2</button>
+          </div>
+          <div className="col-3">
+            <button className="btn" value={3} onClick={() =>seValue(value + "3")}>3</button>
+          </div>
+          <div className="col-3">
+            <button className="btn operators" value="+" onClick={() =>seValue(value + "+")}>+</button>
+          </div>
+        </div>
+        <div className="row buttondiv">
+          <div className="col-3">
+            <button className="btn" value={0} onClick={() =>seValue(value + "0")}>0</button>
+          </div>
+          <div className="col-3">
+            <button className="btn" value="00" onClick={() =>seValue(value + "00")}>00</button>
+          </div>
+          <div className="col-6">
+            <button className="btn btn-primary equal" value="=" onClick={isEqual}>=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
