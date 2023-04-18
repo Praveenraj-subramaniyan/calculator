6c70e3eb958c3acb673fb56f46a50172bec9c2be
spr887011 <spr887011@gmail.com>
2023-04-18 10:04:40 +0530

    Script added in body element

--- src/App.js
+++ src/App.js
@@ -50,77 +50,78 @@ function App() {
  
   return (
     <div className="container-fluid firstContainer">
+      <h1 id="title">Calculator</h1>
       <div className="container calwin">
         <div className="row inputdiv ">
           <div className="col-12">
-            <input className="inputbox " value={value} onKeyDown={(e) => handleKeyDown(e)}/>
+            <input className="inputbox " id="result"  value={value} onKeyDown={(e) => handleKeyDown(e)}/>
           </div>
         </div>
         <div className="row buttondiv">
           <div className="col-3">
-            <button className="btn c" value="c" onClick={onChange}>C</button>
+            <button className="btn c"  id="clear" value="c" onClick={onChange}>C</button>
           </div>
           <div className="col-3">
-            <button className="btn" value="Backspace" onClick={onChange}><i className="fa-solid fa-arrow-left fa-sm"></i></button>
+            <button className="btn"  id="clear" value="Backspace" onClick={onChange}><i className="fa-solid fa-arrow-left fa-sm"></i></button>
           </div>
           <div className="col-3">
-            <button className="btn" value="." onClick={() =>seValue(value + ".")}>.</button>
+            <button className="btn"  id="clear" value="." onClick={() =>seValue(value + ".")}>.</button>
           </div>
           <div className="col-3">
-            <button className="btn operators" value="*" onClick={() =>seValue(value + "*")}>X</button>
-          </div>
-        </div>
-        <div className="row buttondiv">
-          <div className="col-3">
-            <button className="btn" value={7} onClick={() =>seValue(value + "7")}>7</button>
-          </div>
-          <div className="col-3">
-            <button className="btn" value={8} onClick={() =>seValue(value + "8")}>8</button>
-          </div>
-          <div className="col-3">
-            <button className="btn" value={9} onClick={() =>seValue(value + "9")}>9</button>
-          </div>
-          <div className="col-3">
-            <button className="btn operators" value="/" onClick={() =>seValue(value + "/")}>/</button>
+            <button className="btn operators"  id="clear" value="*" onClick={() =>seValue(value + "*")}>X</button>
           </div>
         </div>
         <div className="row buttondiv">
           <div className="col-3">
-            <button className="btn" value={4} onClick={() =>seValue(value + "4")}>4</button>
+            <button className="btn" value={7}  id="clear" onClick={() =>seValue(value + "7")}>7</button>
           </div>
           <div className="col-3">
-            <button className="btn" value={5} onClick={() =>seValue(value + "5")}>5</button>
+            <button className="btn" value={8}  id="clear" onClick={() =>seValue(value + "8")}>8</button>
           </div>
           <div className="col-3">
-            <button className="btn" value={6} onClick={() =>seValue(value + "6")}>6</button>
+            <button className="btn"  id="clear" value={9} onClick={() =>seValue(value + "9")}>9</button>
           </div>
           <div className="col-3">
-            <button className="btn operators" value="-" onClick={() =>seValue(value + "-")}>-</button>
-          </div>
-        </div>
-        <div className="row buttondiv">
-          <div className="col-3">
-            <button className="btn" value={1} onClick={() =>seValue(value + "1")}>1</button>
-          </div>
-          <div className="col-3">
-            <button className="btn" value={2} onClick={() =>seValue(value + "2")}>2</button>
-          </div>
-          <div className="col-3">
-            <button className="btn" value={3} onClick={() =>seValue(value + "3")}>3</button>
-          </div>
-          <div className="col-3">
-            <button className="btn operators" value="+" onClick={() =>seValue(value + "+")}>+</button>
+            <button className="btn operators"  id="clear" value="/" onClick={() =>seValue(value + "/")}>/</button>
           </div>
         </div>
         <div className="row buttondiv">
           <div className="col-3">
-            <button className="btn" value={0} onClick={() =>seValue(value + "0")}>0</button>
+            <button className="btn"  id="clear" value={4} onClick={() =>seValue(value + "4")}>4</button>
           </div>
           <div className="col-3">
-            <button className="btn" value="00" onClick={() =>seValue(value + "00")}>00</button>
+            <button className="btn"  id="clear" value={5} onClick={() =>seValue(value + "5")}>5</button>
+          </div>
+          <div className="col-3">
+            <button className="btn"  id="clear" value={6} onClick={() =>seValue(value + "6")}>6</button>
+          </div>
+          <div className="col-3">
+            <button className="btn operators"  id="subtract" value="-" onClick={() =>seValue(value + "-")}>-</button>
+          </div>
+        </div>
+        <div className="row buttondiv">
+          <div className="col-3">
+            <button className="btn"  id="clear" value={1} onClick={() =>seValue(value + "1")}>1</button>
+          </div>
+          <div className="col-3">
+            <button className="btn"  id="clear" value={2} onClick={() =>seValue(value + "2")}>2</button>
+          </div>
+          <div className="col-3">
+            <button className="btn"  id="clear" value={3} onClick={() =>seValue(value + "3")}>3</button>
+          </div>
+          <div className="col-3">
+            <button className="btn operators"  id="add" value="+" onClick={() =>seValue(value + "+")}>+</button>
+          </div>
+        </div>
+        <div className="row buttondiv">
+          <div className="col-3">
+            <button className="btn"  id="clear" value={0} onClick={() =>seValue(value + "0")}>0</button>
+          </div>
+          <div className="col-3">
+            <button className="btn"  id="clear" value="00" onClick={() =>seValue(value + "00")}>00</button>
           </div>
           <div className="col-6">
-            <button className="btn btn-primary equal" value="=" onClick={isEqual}>=</button>
+            <button className="btn btn-primary equal"  id="equal" value="=" onClick={isEqual}>=</button>
           </div>
         </div>
       </div>
